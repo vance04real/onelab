d0992b258da1ea9737e821f86a85c66d2a031a52
Evans K F Chikuni <vance04real@gmail.com>
2020-05-15 16:39:01 +0200

    add multithreading through asyncTaskExecutor

--- src/main/java/com/persistantcoder/onelab/config/ApplicationConfig.java
+++ src/main/java/com/persistantcoder/onelab/config/ApplicationConfig.java
@@ -4,11 +4,16 @@ import com.persistantcoder.onelab.converters.StringToEnumConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by Evans K F C on May ,2020
@@ -38,8 +43,25 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
         return viewResolver;
     }
 
+
+    @Override
+    protected void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(5000);
+        configurer.setTaskExecutor(asyncTaskExecutor());
+    }
+
+    @Bean
+    public AsyncTaskExecutor asyncTaskExecutor(){
+        ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
+        threadPoolExecutor.setThreadNamePrefix("onelab-thread");
+        return threadPoolExecutor;
+
+
+    }
+
     @Override
     protected void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConverter());
+
     }
 }
