b2aac69fb14f1fbc17ace9756d4402def5dd5a49
Evans K F Chikuni <vance04real@gmail.com>
2020-05-19 09:57:45 +0200

    restcontrollers

---
+++ src/main/java/com/persistantcoder/onelab/controller/Logout.java
@@ -0,0 +1,7 @@
+package com.persistantcoder.onelab.controller;
+
+/**
+ * Created by Evans K F C on May ,2020
+ **/
+public class Logout {
+}
---
+++ src/main/java/com/persistantcoder/onelab/restcontrollers/LoginRestController.java
@@ -0,0 +1,7 @@
+package com.persistantcoder.onelab.restcontrollers;
+
+/**
+ * Created by Evans K F C on May ,2020
+ **/
+public class LoginRestController {
+}
